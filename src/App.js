fc8e373ef69e0055be5e0a6425b7cd507c4edc2d
Henrique Melanda <henrique.melanda@vagas.com.br>
2019-11-11 13:52:02 -0300

    Poc Highcharts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 function App() {
+
+  const [phase, setPhase] = useState({
+    title: {
+      text: 'Candidatos por fase'
+    },
+    credits: {
+      enabled: false
+    },
+    yAxis: {
+      title: false,
+      tickInterval: 500
+    },
+    xAxis: {
+      categories: ['Candidatura', 'Lorem ipsum', 'Dolor sit amet', 'Teste lógica', 'Video entrevista']
+    },
+    legend: false,
+    series: [
+      {
+        data: [2573, 1621, 789, 506, 215],
+        color: '#ef5e30',
+      },
+    ],
+  });
+
+  const [genre, setGenre] = useState({
+    chart: {
+      type: 'area'
+    },
+    title: {
+      text: 'Candidatos por gênero'
+    },
+    legend: false,
+    xAxis: {
+      categories: ['Candidatura', 'Lorem ipsum', 'Dolor sit amet', 'Teste lógica', 'Video entrevista']
+    },
+    yAxis: {
+      title: false,
+      tickInterval: 500
+    },
+    plotOptions: {
+      area: {
+        fillOpacity: 0.5
+      }
+    },
+    credits: {
+      enabled: false
+    },
+    series: [{
+      data: [2573, 1621, 789, 506, 215],
+      color: '#5cceba',
+    }, {
+      data: [1573, 621, 489, 706, 815],
+      color: '#785ef0',
+    }]
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={phase}
+      />
+      <HighchartsReact
+        highcharts={Highcharts}
+        options={genre}
+      />
     </div>
   );
 }
